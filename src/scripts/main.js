a31667f24b5cb8de3223fa56842992406f0c647e
Dave <datadavev@users.noreply.github.com>
2020-06-29 14:29:44 -0400

    Added start stop toggle

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,13 @@ import proj4 from 'proj4';
 //proj4.defs('WGS84', "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees");
 const EPSG_2893 = '+title=NAD83(HARN)/Maryland(ftUS) +proj=lcc +lat_1=39.45 +lat_2=38.3 +lat_0=37.66666666666666 +lon_0=-77 +x_0=399999.9998983998 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs';
 
+const config = {
+  sample_interval: 1000,
+  projection: EPSG_2893
+}
+
+let coord_timeout = null;
+
 window.current_location = {
   timestamp:null,
   x:null,
@@ -23,12 +30,12 @@ window.current_location = {
   altitudeAccuracy: null,
   _dummy:0,
   status: {"bg-error":true, "bg-warning":false, "bg-success":false},
-  running: {}
+  running: false
 }
 
 
 function transformLocation(longitude, latitude) {
-  let xy = proj4(EPSG_2893).forward([longitude, latitude])
+  let xy = proj4(config.projection).forward([longitude, latitude])
   console.debug(xy);
   return xy;
 }
@@ -54,6 +61,11 @@ function statusFromAccuracy(acc) {
   return status;
 }
 
+function touchUI() {
+  let ele = document.getElementById("ui_current_location");
+  ele.__x.$data._dummy = window.current_location._dummy + 1;
+}
+
 function updateCoordinates(position) {
   console.debug(position);
   var timestamp = new Date();
@@ -76,22 +88,31 @@ function updateCoordinates(position) {
   cl.accuracy = position.coords.accuracy;
   cl.altitudeAccuracy = position.coords.altitudeAccuracy;
   cl.status = statusFromAccuracy(cl.accuracy);
-  let ele = document.getElementById("ui_current_location");
-  ele.__x.$data._dummy = cl._dummy + 1;
+  touchUI();
 }
 
-export function getLocation() {
-  navigator.geolocation.getCurrentPosition(updateCoordinates);
-  if (window.current_location._dummy <5 ){
-    setTimeout(getLocation, 1000);
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(updateCoordinates);
+    coord_timeout = setTimeout(getLocation, config.sample_interval);
+}
+
+function startStopLocation() {
+  if (!window.current_location.running) {
+    console.debug("Starting location reporting...")
+    window.current_location.running = true;
+    getLocation();
   } else {
-    window.current_location._dummy = 0;
+    console.debug("Stopping location reporting...")
+    clearTimeout(coord_timeout);
+    window.current_location.running = false;
+    touchUI();
   }
 }
 
 
 window.onload = function() {
-  document.getElementById("bt_start").onclick = getLocation;
+  document.getElementById("bt_start").onclick = startStopLocation;
   let ele = document.getElementById("ui_current_location");
   ele.setAttribute("x-data", "window.current_location");
   ele.setAttribute("x-init", getLocation);
